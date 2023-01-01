adda0414351b7385a714c6a40d5a4b54cb78b777
Michael Girikallo <michael.girikallo@gmail.com>
2023-01-02 00:08:09 +0800

    Implementing dark mode

---
+++ script.js
@@ -0,0 +1,53 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+    let hero = document.getElementById('topsection')
+    if (hero.className === 'topsection') {
+        hero.className = 'dark-mode-top-section';
+    } else {
+        hero.className = 'topsection';
+    }
+
+    let one = document.getElementById('one')
+    if (one.className === 'one') {
+        one.className = 'one-dark-mode';
+    } else {
+        one.className = 'one';
+    } 
+    
+    let list = document.getElementById('list1')
+    if (list.className === 'list') {
+        list.className = 'list-dark-mode';
+    } else {
+        list.className = 'list';
+    } 
+
+    let list2 = document.getElementById('list2')
+    if (list2.className === 'list') {
+        list2.className = 'list-dark-mode';
+    } else {
+        list2.className = 'list';
+    } 
+
+    let list3 = document.getElementById('list3')
+    if (list3.className === 'list') {
+        list3.className = 'list-dark-mode';
+    } else {
+        list3.className = 'list';
+    }
+    
+    let homenav = document.getElementById('homenav')
+    if (homenav.className === 'homenav') {
+        homenav.className = 'homenav-dark-mode';
+    } else {
+        homenav.className = 'homenav';
+    }
+
+    let navbar = document.getElementById('navcolor')
+    if (navbar.className === 'navcolor') {
+        navbar.className = 'navcolor-dark-mode';
+    } else {
+        navbar.className = 'navcolor';
+    }
+}
